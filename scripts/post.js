58068e78ae84d07391d0bac2d2794bb3e920a21d
Matthew Moskalev <web.matthewmoskalev@gmail.com>
2024-05-22 21:02:49 +0300

    feat: Я надеюсь

--- scripts/post.js
+++ scripts/post.js
@@ -1,1 +1,5 @@
-// модуль поста
+document.addEventListener("DOMContentLoaded", function () {
+	document.getElementById("navBlog").addEventListener("click", function () {
+		window.location.href = "/pages/blog.html";
+	});
+});
